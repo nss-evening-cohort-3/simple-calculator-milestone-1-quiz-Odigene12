53973bf20c0f9fa1b1cdb9b751ae2243087c7944
Odigene Joseph <Odigene12@gmail.com>
2016-08-20 10:40:55 -0500

    Expression Class and Test Complete for Chunk 1

--- SimpleCalculator.Tests/ExpressionTest.cs
+++ SimpleCalculator.Tests/ExpressionTest.cs
@@ -6,38 +6,85 @@ namespace SimpleCalculator.Tests
     [TestClass]
     public class ExpressionTest
     {
-        //This test should assess whether I have successfully extracted the terms of the expression.
+        //Make sure you can instantiate a new Expression class
         [TestMethod]
-        public void  IstheTermsExtractionSuccessful()
+        public void MakeANewExpressionClass()
         {
+            Expression newexpress = new Expression();
+
+            Assert.IsNotNull(newexpress);
         }
 
         //This test should assess whether I have successfully extracted the operator in the expression.
         [TestMethod]
-        public void CanISuccessfullyExtractTheOperator()
+        public void DoesTheUserInputMatchExpression()
         {
+            Expression newexpress = new Expression();
+            string userinput = "1 + 2";
+            bool matching = newexpress.MatchCheck(userinput);
+            
+            Assert.IsTrue(matching);
+        }
 
+        //Make sure you can match the expression without spaces
+        [TestMethod]
+        public void DoestTheUserInputMatchWithoutSpaces()
+        {
+            Expression newexpress = new Expression();
+            string userinput = "1+2";
+            bool matching = newexpress.MatchCheck(userinput);
+
+            Assert.IsTrue(matching);
+        }
+
+        //Make sure that the expression evaluates to false if the user inputs something wrong
+        [TestMethod]
+        public void TestingIfUserInputDoesNOTMatch()
+        {
+            Expression newexpress = new Expression();
+            string userinput = "1";
+            bool matching = newexpress.MatchCheck(userinput);
+
+            Assert.IsFalse(matching);
+        }
+
+        //Assess false even if for some reason the user puts just the operator
+        [TestMethod]
+        public void TestingIfItIsJustNumberAndOperator()
+        {
+            Expression newexpress = new Expression();
+            string userinput = "+";
+            bool matching = newexpress.MatchCheck(userinput);
+
+            Assert.IsFalse(matching);
         }
 
         //This test should assess whether I have a Bad Input.
         [TestMethod]
-        public void ThrowAnExceptionIfItIsBad()
+        public void SuccessfulExtraction()
         {
+            Expression newexpress = new Expression();
+            string userinput = "1+2";
+            bool matching = newexpress.MatchCheck(userinput);
+             newexpress.Extracting(userinput);
 
+            Assert.AreEqual(1, newexpress.firstnumber);
+            Assert.AreEqual(2, newexpress.secondnumber);
+            Assert.AreEqual("+", newexpress.theOperator);
         }
 
-        //This test should assess whether I have the right exception thrown.
+        //Make Sure your exception throws for a bad input
         [TestMethod]
-        public void IsThisTheRightException()
+        [ExpectedException(typeof(FormatException))]
+        public void UnsuccessfulExtractionToThrowException()
         {
+            Expression newexpress = new Expression();
+            string userinput = "1+";
+            bool matching = newexpress.MatchCheck(userinput);
+            newexpress.Extracting(userinput);
 
+            
         }
 
-        //This test should assess whether the expression works with and without spaces.
-        [TestMethod]
-        public void SpacesOrNoSpaces()
-        {
-
-        }
     }
 }
--- SimpleCalculator/Expression.cs
+++ SimpleCalculator/Expression.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SimpleCalculator
 {
-    //This class should break down a simple 2-term (i.e: 1+1) calculator expression.
-    class Expression
+    public class Expression
     {
-       //Here is where I am extracting the terms of the expression.
+        
 
-       //Here is where I am extracting the operator ("+", "-", "*", etc.) in the expression. 
+        //Check to make sure the user is inputing the expression correctly
+        public bool MatchCheck(string input)
+        {
+            //Create the pattern that the user must follow when entering his or her expression
+            string pattern = @"^(\d\s*[+\-\*\/\%]\s*\d+)$";
+            Match thismatch = Regex.Match(input, pattern);
 
-       //Here is where an execption error is thrown(hint hint: "throws") whenever there is a BAD input.
+            if (thismatch.Success)
+            {
+                return true;
+            }
+            else
+            {
+                
+                return false;
+            }
+            
+        }
+
+        //If the user made valid expression, extract the operator inside of the expression
+       public void Extracting(string input)
+        {
+            if (MatchCheck(input) == true)
+            {
+                string thispattern = @"^(?<firstnumber>\d+)\s*(?<operator>[\+\-\*\/\%])\s*(?<secondnumber>\d+)$";
+                Match thismatch = Regex.Match(input, thispattern);
+                firstnumber = Convert.ToInt32(thismatch.Groups["firstnumber"].Value);
+                secondnumber = Convert.ToInt32(thismatch.Groups["secondnumber"].Value);
+                theOperator = thismatch.Groups["operator"].Value;
+            }
+            else throw new FormatException("Please enter the correct format");
+        }
+
+        public int firstnumber { get; set; }
+
+        public int secondnumber { get; set; }
+
+        public string theOperator { get; set; }
+
+
+
+
     }
 }
