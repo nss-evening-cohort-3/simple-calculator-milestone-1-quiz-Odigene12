a3c9dc7ad344716ac550d7ace7509e51d95c4aeb
Odigene Joseph <Odigene12@gmail.com>
2016-08-22 17:05:52 -0500

    Working on the program.cs but don't understand how to get the result that I need

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,9 +10,26 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            String prompt = "> ";
+            Evaluation newevaluation = new Evaluation();
+            Expression newexpress = new Expression();
+            Stack forresult = new Stack();
+            Stack forexpress = new Stack();
+            int savedresult = forresult.last;
+            string savedexpress = forexpress.lastq;
+            Counter counter = new Counter();
+            int programcounter = counter.mycounter;
+            string prompt = "[" + programcounter + "]> ";
             Console.Write(prompt);
+            counter.CounterIncrease();
             Console.Read();
+            string userinput = Console.ReadLine().ToLower();
+
+            newexpress.MatchCheck(userinput);
+            newexpress.Extracting(userinput);
+
+            int result = newevaluation.Evaluate(newexpress.firstnumber, newexpress.secondnumber, newexpress.theOperator);
+
+           
         }
     }
 }
---
+++ SimpleCalculator/Stack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Stack
+    {
+        public int last; //This is the last answer that was evaluated.
+
+        public string lastq;  //This is the last expression that was placed into the prompt
+
+        //Create a method that will assign the values of "last" and "lastq" to the results and expressions from the program.cs
+      
+        //store the last answer that came from the program.cs
+        //store the last expression that came from the program.cs
+    }
+}
